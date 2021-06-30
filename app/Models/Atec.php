9aa94349dda8ce12ca8169372ed91664627604e6
suisun <soil_312_1117@yahoo.co.jp>
2021-06-30 15:36:05 +0900

    Get Shop Atec by api

--- app/Models/Atec.php
+++ app/Models/Atec.php
@@ -22,10 +22,14 @@ class Atec extends Model
     }
 
     public static function get_atecs($shop) {
-        $query='select *, DATE_FORMAT(t_atec.created_at,"%Y-%m-%d") as date from t_atec
-                left JOIN (select shop_id, atec_id from t_atec_confirm where shop_id='.$shop.') A on t_atec.id=A.atec_id
-                where shop_id is NULL';
-        $atecs =  DB::select($query);
+        // $query='select *, DATE_FORMAT(t_atec.created_at,"%Y-%m-%d") as date from t_atec
+        //         left JOIN (select shop_id, atec_id from t_atec_confirm where shop_id='.$shop.') A on t_atec.id=A.atec_id
+        //         where shop_id is NULL';
+        // $atecs =  DB::select($query);
+        $atecs = Atec::where('shop_id', null)
+            ->orWhere('shop_id', 0)
+            ->orWhere('shop_id', $shop)
+            ->get();
         return $atecs;
     }
 
