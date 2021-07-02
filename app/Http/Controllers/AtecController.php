dbd0fc7a965794ac406c74843b191d36283d850b
suisun <soil_312_1117@yahoo.co.jp>
2021-07-02 10:40:33 +0900

    Send FCM on Atec Create Event

--- app/Http/Controllers/AtecController.php
+++ app/Http/Controllers/AtecController.php
@@ -74,16 +74,30 @@ class AtecController extends Controller
         else
         {
             $atec = new Atec;
-            // $client = new GuzzleHttp\Client(['base_uri' => 'https://fcm.googleapis.com/fcm/']);
-            // $client->request('POST', 'send', [
-            //     'headers' => [
-            //         'Content-Type' => 'application/json',
-            //         'Authorization' => 'Bearer AAAAI-LPm24:APA91bFfHq8Kp1Gmuo3hiSdoQY6YgAVUVVYPXKENMLLj6Os2nbQ0gL06-YoLOZd9fo2HBMLUVRcKMtO6FcoeT_wGr6B5bTpOrk89jK6IYXaJ9WdTSs7npIiyWjc8xz9NOx2175OTNVhK',
-            //     ],
-            //     'body' => [
-            //         'to' =>
-            //     ]
-            // ]);
+            $shop = $request->input('shop');
+
+            $managers = Manager::where('store', $shop)->where('allow', 1)->get();
+            if ($shop == 0) {
+                $managers = Manager::where('allow', 1)->get();
+            }
+            foreach($managers as $m) {
+                if ($m->fcm_token != null) {
+                    $client = new GuzzleHttp\Client(['base_uri' => 'https://fcm.googleapis.com/fcm/']);
+                    $client->request('POST', 'send', [
+                        'headers' => [
+                            'Content-Type' => 'application/json',
+                            'Authorization' => 'Bearer AAAAI-LPm24:APA91bFfHq8Kp1Gmuo3hiSdoQY6YgAVUVVYPXKENMLLj6Os2nbQ0gL06-YoLOZd9fo2HBMLUVRcKMtO6FcoeT_wGr6B5bTpOrk89jK6IYXaJ9WdTSs7npIiyWjc8xz9NOx2175OTNVhK',
+                        ],
+                        'body' => [
+                            'to' => $m->fcm_token,
+                            'notification' => [
+                                'body' => $request->input('title'),
+                                'title' => $request->input('content'),
+                            ]
+                        ],
+                    ]);
+                }
+            }
         }
 
         $atec->kind = $request->input('kind');
