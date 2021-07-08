9b5ccbf9e5ce732ed59319632c2e0c8409792d68
suisun <soil_312_1117@yahoo.co.jp>
2021-07-08 22:43:53 +0900

    Send Notification on Allow Notice

--- app/Http/Controllers/NoticeApplicationController.php
+++ app/Http/Controllers/NoticeApplicationController.php
@@ -30,6 +30,32 @@ class NoticeApplicationController extends Controller
         $notice->agree = 1;
         $notice->save();
 
+        $shop = Shop::find($notice->shop_id);
+        $customers = $shop->customers;
+
+        foreach($customers as $m) {
+            if ($m->fcm_token != null) {
+                $client = new Client(['base_uri' => 'https://fcm.googleapis.com/fcm/']);
+                $client->request('POST', 'send', [
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                        'Authorization' => 'Bearer AAAAI-LPm24:APA91bFfHq8Kp1Gmuo3hiSdoQY6YgAVUVVYPXKENMLLj6Os2nbQ0gL06-YoLOZd9fo2HBMLUVRcKMtO6FcoeT_wGr6B5bTpOrk89jK6IYXaJ9WdTSs7npIiyWjc8xz9NOx2175OTNVhK',
+                    ],
+                    'json' => [
+                        'to' => $m->fcm_token,
+                        'data' => [
+                            'type' => 'notify',
+                            'notify' => $notice->id,
+                        ],
+                        'notification' => [
+                            'body' => $request->input('title'),
+                            'title' => $request->input('content'),
+                        ]
+                    ],
+                ]);
+            }
+        }
+
         return redirect("/notice_application");
     }
 }
