a2d665011eb381e915b3cc576ac451dc3296bf45
suisun <soil_312_1117@yahoo.co.jp>
2021-07-26 13:58:04 +0900

    Add Admin Master

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -88,6 +88,7 @@ class AdminController extends Controller
     public function update(Request $request, $id)
     {
         //
+        dd($id);
         $admin = User::find($id);
         $admin->name = $request->input('name');
         $admin->email = $request->input('email');
