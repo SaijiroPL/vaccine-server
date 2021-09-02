e9dbf6b3124e3a550ef318c718aef2c27019eef4
suisun <soil_312_1117@yahoo.co.jp>
2021-09-02 13:53:36 +0900

    Split goods into types

--- resources/views/goods.blade.php
+++ resources/views/goods.blade.php
@@ -11,7 +11,7 @@
     <div class="m-portlet__body">
         <div class="row">
             <div class="col-md-12 m--padding-bottom-15">
-              <a href="{{ url('/master/carrying_goods') }}" class="btn btn-primary">
+              <a href="{{ url('/master/carrying_goods') }}" class="btn @if ($type == null) btn-primary @endif">
                 <span>
                   <span>&nbsp;&nbsp;すべて&nbsp;&nbsp;</span>
                 </span>
